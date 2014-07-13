4c4218a2a730ef796d34b3d26553453ede8f2c0c
Jason Shen <sikajs@gmail.com>
2014-07-13 09:11:53 +0700

    save files for use in other place

--- spec/features/create_ticket_spec.rb
+++ spec/features/create_ticket_spec.rb
@@ -5,6 +5,7 @@ feature "Create Tickets" do
     project = FactoryGirl.create(:project)
     user = FactoryGirl.create(:user)
     define_permission!(user, :view, project)
+    define_permission!(user, "create tickets", project)
     @email = user.email
     sign_in_as!(user)
 
