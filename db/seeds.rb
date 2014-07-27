cbf59b0b7a5116306ba0ac1b11029c49cc807995
Jason Shen <sikajs@gmail.com>
2014-07-27 18:08:57 +0800

    Add a seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,11 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+  admin_user = User.create(email: "admin@example.com",
+                           name: "admin",
+                           password: "password",
+                           password_confirmation: "password",
+                           admin: true)
   user = User.create(name: 'example user', email: 'example@example.com', password:'test1234', password_confirmation: 'test1234', admin: true)
-  project = Project.create(name: 'example', description: 'example description')
+  project = Project.create(name: 'Ticketee Beta')
   ticket = Ticket.create(title: 'test ticket', description: 'test ticket description', project_id: project.id, user_id: user.id)
