b1a47a77ddf68f878b703e08177187c124b23b28
Jason Shen <sikajs@gmail.com>
2014-08-06 22:13:40 +0800

    Uploaded Assets are now strictly served through the FilesController

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,8 @@ Ticketee::Application.routes.draw do
     resources :tickets
   end
 
+  resources :files
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
